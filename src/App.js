1fd87187929ba07ac9bfb120a75cb77564163ad7
lukewtom93 <lukewtom93@gmail.com>
2026-03-29 15:41:46 +0100

    it works!

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import './api/axiosDefaults';
+import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
-import SignInForm from "./pages/auth/SIgnInForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
